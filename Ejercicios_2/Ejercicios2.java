787d59ec89a8d98445d04c75313c24d767e4a257
PINGUIS37 <109268745+PINGUIS37@users.noreply.github.com>
2023-04-02 12:08:59 -0600

    Add files via upload
    9 ejercicios =)

---
+++ Ejercicios_2/Ejercicios2.java
@@ -0,0 +1,109 @@
+/**************************************
+ * Autor: Yael Alberto Gómez Hernández*
+ * Fecha Creación: 01/04/23           *
+ * Fecha Actualización:	01/04/23	  *
+ * Descripción: Lista de ejercicios 2 *
+ *************************************/
+package Ejercicios_2;
+import java.util.Scanner;
+public class Ejercicios2 {
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+		Scanner cad = new Scanner(System.in);
+		
+		/*//1*************************************************************************
+		int numHoras;
+		int costeHora;
+		
+		System.out.print("Inserte el número de horas que trabaja: ");
+		numHoras = ent.nextInt();
+		System.out.print("Inserte el coste por hora: ");
+		costeHora = ent.nextInt();
+		System.out.println("La paga total es: " + (numHoras * costeHora));
+		
+		//2*************************************************************************
+		float x = 4;
+		float y = 3;
+		float a = 2;
+		float b = 1;
+		
+		System.out.println("(x+y)/(a*b)= " + (x-y)/(a*b));						  
+		
+		//3*************************************************************************
+		float x = 2;
+		float y = 3;
+		float z = 4;
+
+		System.out.println("El resultado de la operación √((x + y * z)^5) es: " + Math.sqrt(Math.pow((x+y*z),5)));
+		
+		//4*************************************************************************
+		float x;
+		float y;
+		
+		System.out.println("Inserte el número 1: ");
+		x = ent.nextInt();
+		System.out.println("Inserte el número 2: ");
+		y = ent.nextInt();
+		System.out.println(x + " entre " + y + " da un cociente " + (x/y) + " y un residuo " + (x%y));
+		
+		//5*************************************************************************
+		float x;
+		float y;
+		
+		System.out.print("Inserte el cateto adyacente: ");
+		x = ent.nextFloat();
+		System.out.print("Inserte el cateto adyacente: ");
+		y = ent.nextFloat();
+		System.out.println("La longitud de la hipotenusa es: " + Math.sqrt(Math.pow(x,2)+Math.pow(y,2)));
+		
+		//6*************************************************************************
+		int x;
+		String cadena;
+		
+		System.out.print("Inserte el número: ");
+		x = ent.nextInt();
+		cadena = Integer.toString(x);
+		for (int i = cadena.length(); i > 0; i--) {
+			System.out.println(cadena.charAt(i));
+			
+		}																		  
+		
+		//7*************************************************************************
+		int pesoP = 112;
+		int pesoM = 75;
+		int numP;
+		int numM;
+		
+		System.out.print("Inserte el número de payasos vendidos: ");
+		numP = ent.nextInt();
+		System.out.print("Inserte el número de muñecas vendidas: ");
+		numM = ent.nextInt();
+		System.out.println("El peso total del paquete es: " + (pesoP*numP + pesoM*numM));
+		
+		//8*************************************************************************
+		int distancia;
+		int tiempo;
+		
+		System.out.print("Inserte la distancia recorrida en metros: ");
+		distancia = ent.nextInt();
+		System.out.print("Inserte el tiempo recorrido en segundos: ");
+		tiempo = ent.nextInt();
+		System.out.println("La velocidad es de: " + distancia/tiempo + " m/s");  
+		
+		//9*************************************************************************
+		double a;
+		double b;
+		double c;
+		
+		System.out.print("Inserte el valor de A: ");
+		a = ent.nextDouble();
+		System.out.print("Inserte el valor de B: ");
+		b =ent.nextDouble();
+		System.out.print("Inserte el valor de C: ");
+		c = ent.nextDouble();
+		
+		System.out.println("X1= " + ((-b) + Math.sqrt(Math.pow(b, 2)-(4*a*c)))/(2*a));
+		System.out.println("X2= " + ((-b) - Math.sqrt(Math.pow(b, 2)-(4*a*c)))/(2*a));*/
+
+	}
+}
